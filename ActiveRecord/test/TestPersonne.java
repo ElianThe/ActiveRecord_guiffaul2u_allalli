198be1699862fcb280a48e12779a541441d3139a
Mehdi Allali <mehdi.allali8@orange.fr>
2022-11-23 09:15:49 +0100

    ajout des BeforeEach et AfterEach

--- ActiveRecord/test/TestPersonne.java
+++ ActiveRecord/test/TestPersonne.java
@@ -1,2 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.sql.SQLException;
+
 public class TestPersonne {
+
+    @BeforeEach
+    public void initTable() throws SQLException {
+        // Utilisation de la méthode statique createTable
+        Personne.createTable();
+        // On crée des nouvelles Personne qu'on ajoute à la table
+        (new Personne("Spielberg", "Steven")).save();
+        (new Personne("Scott", "Ridley")).save();
+        (new Personne("Kubrick","Stanley")).save();
+        (new Personne("Fincher", "David")).save();
+    }
+
+    @AfterEach
+    public void deleteTable() throws SQLException {
+        // Utilisation de la méthode statique deleteTable
+        Personne.deleteTable();
+    }
 }
+
