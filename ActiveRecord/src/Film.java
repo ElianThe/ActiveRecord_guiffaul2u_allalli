ffb97d12efde167f686b23484bf86067897e2413
Mehdi Allali <mehdi.allali8@orange.fr>
2022-11-23 09:38:34 +0100

    ajout des constructeurs de Film

--- ActiveRecord/src/Film.java
+++ ActiveRecord/src/Film.java
@@ -4,6 +4,13 @@ public class Film {
 
     public Film(String titre, Personne real){
         this.titre = titre;
-        this.id_real = real.
+        this.id_real = real.getId();
+        this.id = -1;
+    }
+
+    public Film(String titre, int id, int id_real){
+        this.titre = titre;
+        this.id = id;
+        this.id_real = id_real;
     }
 }
